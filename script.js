b2ea1fcf79d174378713afb02d20671dff7dc7f5
Kevin Hsieh <khsieh95@gmail.com>
2020-12-16 18:07:27 -0800

    add click functions for buttons

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("hi kevin");
+
+var currentDay = $("#currentDay");
+var today = moment().format("MMM Do YY");
+var saveButtonsClicked = $(".saveBtn");
+var userInput = JSON.parse(localStorage.getItem("userInput") || "[]");
+var textAreas = $("textarea");
+
+$("#currentDay").text(today);
+console.log(today);
+
+// function for save button
+saveButtonsClicked.on("click", function () {
+  console.log("clicked");
+});

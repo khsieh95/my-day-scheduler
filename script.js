9bf7995873da5cf061f63dfc8e47d5bfd3a6d0e1
Kevin Hsieh <khsieh95@gmail.com>
2020-12-16 19:06:35 -0800

    create function and if statements for styling page with reference to time

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-console.log("hi kevin");
-
+// Global Variables
 var currentDay = $("#currentDay");
 var today = moment().format("MMM Do YY");
 var saveButtonsClicked = $(".saveBtn");
-var userInput = JSON.parse(localStorage.getItem("userInput") || "[]");
 var textAreas = $("textarea");
+var userInput = JSON.parse(localStorage.getItem("userInput") || "[]");
+var hourOfDay = $("#currentHour");
+var currentHour = moment().hours();
 
 $("#currentDay").text(today);
 console.log(today);
@@ -12,4 +13,25 @@ console.log(today);
 // function for save button
 saveButtonsClicked.on("click", function () {
   console.log("clicked");
+});
+
+// function to compare local time to time on calendar
+
+$.each(textAreas, function () {
+  var dataTime = this.getAttribute("data-time");
+  if (parseInt(dataTime) < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("present future");
+  }
+
+  if (parseInt(dataTime) === currentHour) {
+    $(this).removeClass("past future");
+    $(this).addClass("present");
+  }
+
+  if (parseInt(dataTime) > currentHour) {
+    $(this).removeClass("past present");
+
+    $(this).addClass("future");
+  }
 });

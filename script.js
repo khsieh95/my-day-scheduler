bad0145876321e1b6a5c0c9798f36b03250bcf71
Kevin Hsieh <khsieh95@gmail.com>
2020-12-16 19:43:30 -0800

    locally store from save button

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ var today = moment().format("MMM Do YY");
 var saveButtonsClicked = $(".saveBtn");
 var textAreas = $("textarea");
 var userInput = JSON.parse(localStorage.getItem("userInput") || "[]");
-var hourOfDay = $("#currentHour");
 var currentHour = moment().hours();
 
 $("#currentDay").text(today);
-console.log(today);
 
-// function for save button
+// function for save button and to locally store
 saveButtonsClicked.on("click", function () {
   console.log("clicked");
+  var userText = $(this.previousElementSibling).val();
+  var parentTime = $(this).parent().attr("id");
+  localStorage.setItem(parentTime, userText);
+  console.log(localStorage);
 });
 
 // function to compare local time to time on calendar
@@ -22,16 +24,11 @@ $.each(textAreas, function () {
   if (parseInt(dataTime) < currentHour) {
     $(this).addClass("past");
     $(this).removeClass("present future");
-  }
-
-  if (parseInt(dataTime) === currentHour) {
+  } else if (parseInt(dataTime) === currentHour) {
     $(this).removeClass("past future");
     $(this).addClass("present");
-  }
-
-  if (parseInt(dataTime) > currentHour) {
+  } else {
     $(this).removeClass("past present");
-
     $(this).addClass("future");
   }
 });
